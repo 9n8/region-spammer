68aef39ebf392bce11a07b01fc1863f940a44c40
pw#1337 <87565211+herluvhr@users.noreply.github.com>
2021-08-20 22:33:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+
+os.system(f'title [REGION SPAMMER]')
+os.system(f'mode 75,24')
+
+import colorama
+from colorama import Fore
+
+token = input(f"{Fore.MAGENTA}Token :{Fore.CYAN} ")
+id = input(f"{Fore.MAGENTA}GC ID :{Fore.CYAN} ")
+
+os.system('cls')
+
+import requests
+
+vc = "https://discord.com/api/v9/channels/"+str(id)+"/call"
+msg = "https://discord.com/api/v9/channels/"+str(id)+"/messages"
+
+m = {
+    'content': 'hi'
+}
+
+headers = {
+    'Authorization': str(token)
+}
+
+requests.post(msg, headers=headers, json=m)
+
+region = [
+    'hongkong',
+    'europe',
+    'brazil',
+    'us-central',
+    'us-east',
+    'sydney',
+    'russia',
+    'india',
+    'japan',
+    'us-west',
+    'us-south',
+    'singapore'
+]
+
+print(f'{Fore.CYAN}                             By: {Fore.MAGENTA}pw#1337')
+
+import random
+
+while True:
+    rgc = random.choice(region)
+    payload = {
+        'region': str(rgc)
+    }
+
+    requests.patch(vc, json = payload, headers=headers)
+    print(f"{Fore.LIGHTGREEN_EX}Region: "+str(rgc))
